a129c55c969d3286d5b6ed8b63a3eb70323e466f
Deepak Awasthi <awasthi12deepak@gmail.com>
2022-01-25 20:04:46 +0530

    Static List Structure Created

--- src/App.js
+++ src/App.js
@@ -1,51 +1,42 @@
-import ExpenseItem from "./components/ExpenseItem";
+import React from "react";
+
+import Expenses from "./components/Expenses";
+import "./index.css";
 
 function App() {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
+      title: "Chocolate",
+      amount: 100,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 25000, date: new Date(2021, 2, 12) },
     {
       id: "e3",
       title: "Car Insurance",
-      amount: 294.67,
+      amount: 2100,
       date: new Date(2021, 2, 28),
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
-      amount: 450,
+      amount: 7800,
       date: new Date(2021, 5, 12),
     },
   ];
 
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Get Started"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-      <h2>Get Started</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <h2 className="start">Get Started</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
